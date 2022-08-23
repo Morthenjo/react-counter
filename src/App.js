a2b1a06bf0dd322b9f206617cc87e3ffc51751ce
MorthenJo <MorthenJo.kodehode@gmail.com>
2022-08-23 11:00:52 +0200

    bruh

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+const Timer = () => {
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+
+  function toggle() {
+    setIsActive(!isActive);
+  }
+
+  function reset() {
+    setSeconds(0);
+    setIsActive(false);
+  }
+
+  function add() {
+    setSeconds(seconds + 1);
+  }
+
+  function substract() {
+    setSeconds(seconds - 1);
+  }
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds((seconds) => seconds + 1);
+      }, 1000);
+    } else if (!isActive && seconds !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, seconds]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app">
+      <h1>Counter</h1>
+      <h1 className="time">{seconds}s</h1>
+      <div className="row">
+        <button
+          className={`button start start-${isActive ? "active" : "inactive"}`}
+          onClick={toggle}
         >
-          Learn React
-        </a>
-      </header>
+          {isActive ? "Pause" : "Start"}
+        </button>
+        <button className="button" onClick={reset}>
+          Reset
+        </button>
+      </div>
+      <div>
+        <button className="button add" onClick={add}>
+          Add
+        </button>
+        <button className="button substract" onClick={substract}>
+          Substract
+        </button>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default Timer;
